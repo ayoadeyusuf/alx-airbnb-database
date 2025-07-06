7e806f2626f02c7b5566a9eb78516976c5357108
Ayoade Yusuf <ayoadeyusuf98@gmail.com>
2025-07-06 11:01:19 +0300

    Create performance.sql

---
+++ database-adv-script/performance.sql
@@ -0,0 +1,23 @@
+SELECT
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.price_per_night,
+    pay.payment_id,
+    pay.amount AS payment_amount,
+    pay.payment_date,
+    pay.payment_method
+FROM
+    bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
