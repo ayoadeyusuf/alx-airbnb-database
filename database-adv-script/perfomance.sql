2be5f66dc0bfd8a223775fe60def8e5f12c3eb82
Ayoade Yusuf <ayoadeyusuf98@gmail.com>
2025-07-06 13:42:06 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -25,5 +25,37 @@ WHERE
     b.status = 'confirmed'
 ORDER BY
     b.start_date DESC
+LIMIT 100; 
+
+-- AND
+
+EXPLAIN ANALYZE
+SELECT
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.price_per_night,
+    pay.payment_id,
+    pay.amount AS payment_amount,
+    pay.payment_date,
+    pay.payment_method
+FROM
+    bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+WHERE
+    b.status = 'confirmed'
+ORDER BY
+    b.start_date DESC
 LIMIT 100;
 
