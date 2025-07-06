1a11db955720edc0b261367b7422526d620e5f5b
Ayoade Yusuf <ayoadeyusuf98@gmail.com>
2025-07-06 11:09:10 +0300

    Update performance.sql

--- database-adv-script/performance.sql
+++ database-adv-script/performance.sql
@@ -20,4 +20,10 @@ FROM
     bookings b
 JOIN users u ON b.user_id = u.user_id
 JOIN properties p ON b.property_id = p.property_id
-LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+WHERE
+    b.status = 'confirmed'
+ORDER BY
+    b.start_date DESC
+LIMIT 100;
+
